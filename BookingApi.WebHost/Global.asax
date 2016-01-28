24efe19b056eff8d740f760667de44aff61dffb0
Andrey Piterov <pfm@live.ru>
2016-01-28 23:03:38 +0300

    init

---
+++ BookingApi.WebHost/Global.asax
@@ -0,0 +1,1 @@
+<%@ Application Inherits="Piter.Samples.Booking.HttpApi.HttpHost.Global "%>
---
+++ BookingApi.WebHost/Infrastructure.fs
@@ -0,0 +1,14 @@
+﻿namespace Piter.Samples.Booking.HttpApi.HttpHost
+
+open System
+open System.Web.Http
+
+type HttpRoutesDefaults = { Controller: string; Id : obj }
+
+type Global() =
+    inherit System.Web.HttpApplication()
+    member this.Application_Start ( sender : obj ) ( e : EventArgs ) = 
+        GlobalConfiguration.Configuration.Routes.MapHttpRoute(
+            "DeafaultAPI",
+            "{controller}/{id}",
+            { Controller = "Home"; Id = RouteParameter.Optional }) |> ignore
